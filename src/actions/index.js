c6089042177c828a2cee00b40651089fcd273ac1
Dan Shafie <danshafie@gmail.com>
2016-04-29 13:25:02 -0700

    added fetch weather action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,19 @@
+import axios from 'axios';
 
 const API_KEY = '0895023bf88999e39e32b5d035fe4ecf';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+export function fetchWeather(city){
+
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
